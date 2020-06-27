8b173116acec17591d816a238805c14c4992b7e5
elvinsomon <elvinsomon2001@gmail.com>
2020-06-27 12:51:07 -0400

    Agregado el algoritmo de burbuja [Funcional]

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//Los jugadores generalmente se sientan en círculo. El jugador designado para ir primero dice el número "1", y cada jugador cuenta un número por turno. Sin embargo, cualquier número divisible por tres se reemplaza por la palabra "Marvel" y cualquier número divisible por cinco por la palabra "DC". Los números divisibles por 3 y 5 se convierten en "Marvel - DC". Un jugador que duda o comete un error es eliminado del juego.
-
 //var jugadores = new array[40]();
 
 for (let x = 1; x < 40; x++) {
@@ -13,3 +11,48 @@ for (let x = 1; x < 40; x++) {
 		console.log(' DC');
 	}
 }
+
+var nums = [10, 80, 70, 44, 78, 5, 4];
+
+//Ordene el array anterior de la siguiente forma  = {4,5,10,44,70,78,80}
+
+while (
+	nums[0] > nums[1] ||
+	nums[1] > nums[2] ||
+	nums[2] > nums[3] ||
+	nums[3] > nums[4] ||
+	nums[4] > nums[5] ||
+	nums[5] > nums[6]
+) {
+	if (nums[5] > nums[6]) {
+		let nuevosValores = cambio(nums[5], nums[6]);
+		nums[5] = nuevosValores[0];
+		nums[6] = nuevosValores[1];
+	} else if (nums[4] > nums[5]) {
+		let nuevosValores = cambio(nums[4], nums[5]);
+		nums[4] = nuevosValores[0];
+		nums[5] = nuevosValores[1];
+	} else if (nums[3] > nums[4]) {
+		let nuevosValores = cambio(nums[3], nums[4]);
+		nums[3] = nuevosValores[0];
+		nums[4] = nuevosValores[1];
+	} else if (nums[2] > nums[3]) {
+		let nuevosValores = cambio(nums[2], nums[3]);
+		nums[2] = nuevosValores[0];
+		nums[3] = nuevosValores[1];
+	} else if (nums[1] > nums[2]) {
+		let nuevosValores = cambio(nums[1], nums[2]);
+		nums[1] = nuevosValores[0];
+		nums[2] = nuevosValores[1];
+	} else if (nums[0] > nums[1]) {
+		let nuevosValores = cambio(nums[0], nums[1]);
+		nums[0] = nuevosValores[0];
+		nums[1] = nuevosValores[1];
+	}
+}
+
+function cambio(pos1, pos2) {
+	return [pos2, pos1];
+}
+
+console.log(nums);
